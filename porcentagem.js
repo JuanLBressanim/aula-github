2309fc46363ca0e89364343aa8ab301ea2a6370f
JuanLBressanim <juanlbressanim@gmail.com>
2024-08-19 22:07:48 -0300

    começando o algoritmo para converter porcentagens

---
+++ porcentagem.js
@@ -0,0 +1,8 @@
+var valor=parseFloat(prompt("Digite o valor a converter"));
+var porcentagem=parseInt(prompt("Digite a porcentagem que deseja encontrar"));
+//converter o número inteiro para decimal 
+//só é possível achar a % usando núemro decimal
+var porcentagemDecimal=porcentagem/100;
+//multiplica o valor para porcentagem para encontrar o resultado
+var resultado=valor*porcentagemDecimal;
+console.log("O resultado é:");
