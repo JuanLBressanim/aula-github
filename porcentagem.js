1be344a85193fc25fac7ba4bffbd7c8edef5a5ab
JuanLBressanim <juanlbressanim@gmail.com>
2024-08-19 22:25:29 -0300

    inicio do algoritmo de porcentagem de descontos para produtos

--- porcentagem.js
+++ porcentagem.js
@@ -2,7 +2,7 @@ var valor=parseFloat(prompt("Digite o valor a converter"));
 var porcentagem=parseInt(prompt("Digite a porcentagem que deseja encontrar"));
 //converter o número inteiro para decimal 
 //só é possível achar a % usando núemro decimal
-var porcentagemDecimal=porcentagem/100;
+var porcentagemDecimal=porcentagem /100;
 //multiplica o valor para porcentagem para encontrar o resultado
-var resultado=valor*porcentagemDecimal;
+var resultado=valor* porcentagemDecimal;
 console.log("O resultado é:");
---
+++ porcentagemProduto.js
@@ -0,0 +1,5 @@
+var produto=prompt("Qual é seu produto");
+var quantidade=parseInt(prompt("Quantos prdoutos foram vendidos"));
+var valorProdutos=parseFloat(prompt("Qual o valor deste produto?"));
+var valorTotal=quantidade-valorProdutos;
+var desconto=parseFloat(prompt("Digite o valor de desconto"));
