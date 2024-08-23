dde92d78203c5a41fb9b083b1b8ae351e33f78c8
JuanLBressanim <juanlbressanim@gmail.com>
2024-08-22 21:25:09 -0300

    continuação do algoritmo

--- porcentagemProduto.js
+++ porcentagemProduto.js
@@ -3,3 +3,6 @@ var quantidade=parseInt(prompt("Quantos prdoutos foram vendidos"));
 var valorProdutos=parseFloat(prompt("Qual o valor deste produto?"));
 var valorTotal=quantidade-valorProdutos;
 var desconto=parseFloat(prompt("Digite o valor de desconto"));
+var descontoTotal=porcentagemDes /100;
+var preço=valorProdutos* descontoTotal;
+console.log("o preço do produto é:" *preço);
